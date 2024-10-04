c25bfcc8de02c3840810015f3cc3e9619af842a1
Finlay Gress <finlay.gress@gmail.com>
2024-10-04 02:10:35 +0200

    os env support

--- config/config.go
+++ config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"os"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
@@ -14,6 +16,25 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
+
+	apiKey := os.Getenv("api_key")
+	ddns_comment := os.Getenv("ddns_comment")
+	zones := os.Getenv("zones")
+	updateInterval := os.Getenv("update_interval")
+
+	zonesArray := strings.Split(zones, ",")
+
+	x, _ := strconv.Atoi(updateInterval)
+
+	if apiKey != "" && ddns_comment != "" && updateInterval != "" && zones != "" {
+		return &Config{
+			ApiKey:         apiKey,
+			DDNSComment:    ddns_comment,
+			Zones:          zonesArray,
+			UpdateInterval: x,
+		}, nil
+	}
+
 	config := &Config{}
 	yamlData, err := os.ReadFile("config.yml")
 	if err != nil {
